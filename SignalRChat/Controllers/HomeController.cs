98ec6b91f559849ece1b991d9452c0b6f43d669d
Marine Lesigne <marine.lesigne@excense.fr>
2019-06-14 16:12:07 +0200

    Add project files.

---
+++ SignalRChat/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public class ChatEditViewModel
+        {
+            public string Id { get; set; }
+        }
+
+    
+        public ActionResult Chat(string id)
+        {
+            return View(new ChatEditViewModel() { Id = id});
+        }
+    }
+}
