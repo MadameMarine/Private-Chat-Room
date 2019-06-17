c25aeb2d88bd3f7806ddd5307c37c23a58f46d1d
Marine Lesigne <marine.lesigne@excense.fr>
2019-06-17 16:35:21 +0200

    Room crées, les messages s'envoient dans les rooms dédiées Code plus lisible

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -6,17 +6,56 @@ namespace SignalRChat
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string name, string message) //ou void
+        public async Task Send(string groupId, string name, string message)
         {
             // Call the addNewMessageToPage method to update clients.
-            await Clients.All.addNewMessageToPage(new ChatMessage() { Name = name, Message = message });
+            await Clients.Group(groupId).addNewMessageToPage(new ChatMessage() {Name = name, Message = message });
         }
 
         public class ChatMessage
         {
+           public string GroupChatId { get; set; }
             public string Name { get; set;}
             public string Message { get; set;}
         }
+
+        //ajoute utilisateur dans un groupe
+        public async Task JoinRoom(string GroupChatId)
+        {
+            await Groups.Add(Context.ConnectionId, GroupChatId);
+            //Clients.Group(GroupChatId).addChatMessage(Context.User.Identity.Name + " joined.");
+        }
+
+        //provisoire
+        public void JoinGroup(string GroupChatId)
+        {
+            this.Groups.Add(this.Context.ConnectionId, GroupChatId);
+        }
+
+        //supprime un utilisateur
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.Remove(Context.ConnectionId, roomName);
+        }
+
+        //envoi au groupe
+        public async Task SendToGroup(string GroupChatId,string name, string message)
+        {
+            await Clients.Group(GroupChatId).Send("addNewMessageToPage",name, message);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         //public Task JoinGroup(string group)
@@ -28,7 +67,7 @@ namespace SignalRChat
         //{
         //    return Clients.Group(group).SendAsync("ReceiveMessage", message);
         //}
-        
+
 
 
     }
