2375f7527d96f6ce85f4c9719a45488f566f83e6
Marine Lesigne <marine.lesigne@excense.fr>
2019-06-21 16:51:49 +0200

    Rajout messageTextBox et button Send UWP s'envoit des messages : OK

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -36,19 +36,22 @@ namespace App1
            
         }
 
-        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        //private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
             myHubConnection = new HubConnection("http://localhost:52527");
             myProxy = myHubConnection.CreateHubProxy("chatHub");
 
-            
+
 
             //Get informations from browser
             myProxy.On("addNewMessageToPage", message =>
             {
                 _ = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
                 {
-                    messages.Text += message.Username + ": " + message.Message;
+
+                    messages.Text += message.Name + ": " + message.Message;
+
                 });
             }); ;
         }
@@ -59,25 +62,27 @@ namespace App1
         {
             userTextbox.IsEnabled = false;
             joinButton.IsEnabled = false;
+            messageTextBox.IsEnabled = true;
+            envoyerButton.IsEnabled = true;
 
             if (myHubConnection.State != ConnectionState.Connected)
             {
                 Console.WriteLine("Compositeur is connecting to server...");
                 await myHubConnection.Start();
-
+                
             }
 
             //Join la room            
             Console.WriteLine("Compositeur joining group Chat/Compositeur...");
             await myProxy.Invoke("joinGroup", "Compositeur");
-            Console.WriteLine("Compositeur group joined");
+            Console.WriteLine("Compositeur group joined");         
 
-            //TOTO : rendre visible une textbox pour envoyer un message
+        }
 
-            await myProxy.Invoke("Send", "Compositeur", userTextbox.Text, "salut");
-           
-
-
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO : rendre visible une textbox pour envoyer un message
+            await myProxy.Invoke("Send", "Compositeur", userTextbox.Text, messageTextBox.Text);
         }
     }
 }
